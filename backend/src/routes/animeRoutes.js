5e8aaf8ad08d9c987a5f90470af86a659be8e9f2
HKMUSIC <himanshu4hk9@gmail.com>
2025-10-04 14:54:43 +0530

    Update animeRoutes.js

--- backend/src/routes/animeRoutes.js
+++ backend/src/routes/animeRoutes.js
@@ -1,1 +1,11 @@
+import express from "express";
+import { getAllAnime, addAnime, deleteAnime } from "../controllers/animeController.js";
+import { verifyToken } from "../middlewares/verifyToken.js";
 
+const router = express.Router();
+
+router.get("/", getAllAnime);
+router.post("/", verifyToken, addAnime);
+router.delete("/:id", verifyToken, deleteAnime);
+
+export default router;

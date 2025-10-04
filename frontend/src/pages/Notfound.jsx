bc10d442813ff776cbdcf5b6949310482642321f
HKMUSIC <himanshu4hk9@gmail.com>
2025-10-04 14:43:09 +0530

    Create Notfound.jsx

---
+++ frontend/src/pages/Notfound.jsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="h-[80vh] flex items-center justify-center flex-col">
+      <h1 className="text-4xl font-bold text-orange-500">404</h1>
+      <p className="text-gray-400 mt-2">Page Not Found</p>
+    </div>
+  );
+}

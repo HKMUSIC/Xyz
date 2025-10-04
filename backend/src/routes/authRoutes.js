d9e2aababbfa6ce211672edb00b9045584917291
HKMUSIC <himanshu4hk9@gmail.com>
2025-10-04 14:55:13 +0530

    Update authRoutes.js

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,1 +1,10 @@
+import express from "express";
+import { verifyAdmin } from "../controllers/authController.js";
+import { verifyToken } from "../middlewares/verifyToken.js";
 
+const router = express.Router();
+
+// Protected route to verify admin access
+router.get("/verify", verifyToken, verifyAdmin);
+
+export default router;

341c08c9bc93560b61df7d69f94620563058559b
HKMUSIC <himanshu4hk9@gmail.com>
2025-10-04 14:52:01 +0530

    Update verifyToken.js

--- backend/src/middlerwares/verifyToken.js
+++ backend/src/middlerwares/verifyToken.js
@@ -1,1 +1,15 @@
+import { auth } from "../utils/firebaseAdmin.js";
 
+export const verifyToken = async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) return res.status(401).json({ error: "Unauthorized" });
+
+  try {
+    const decoded = await auth.verifyIdToken(token);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(403).json({ error: "Invalid or expired token" });
+  }
+};

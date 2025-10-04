f3326e3c0cbffb1715086d3f9f67e4e788bcef04
HKMUSIC <himanshu4hk9@gmail.com>
2025-10-04 14:38:16 +0530

    Create Footer.jsx

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-[#121212] text-gray-400 text-center py-4 border-t border-gray-800">
+      © 2025 𝑫𝑹𝑿 𝑨𝑵𝑰𝑴𝑬 | All Rights Reserved
+    </footer>
+  );
+}

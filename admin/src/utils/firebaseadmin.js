6eb4cf86b72557171b24913d38314177d90facc6
HKMUSIC <himanshu4hk9@gmail.com>
2025-10-04 14:58:48 +0530

    Update firebaseadmin.js

--- admin/src/utils/firebaseadmin.js
+++ admin/src/utils/firebaseadmin.js
@@ -1,1 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyD2WpwNAE87gAJD0T3b60U0o24m4Aol6BI",
+  authDomain: "drx-anime.firebaseapp.com",
+  projectId: "drx-anime",
+  storageBucket: "drx-anime.appspot.com",
+  messagingSenderId: "1046704841991",
+  appId: "1:1046704841991:web:0435ce22301b6465c191a2"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);

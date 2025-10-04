8ce388388d3b3eb4b3902314142fbb3436c6e66c
HKMUSIC <himanshu4hk9@gmail.com>
2025-10-04 14:43:40 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import AnimeDetail from "./pages/AnimeDetail";
+import Watch from "./pages/Watch";
+import NotFound from "./pages/NotFound";
 
+export default function App() {
+  return (
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/anime/:id" element={<AnimeDetail />} />
+            <Route path="/watch/:id" element={<Watch />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
